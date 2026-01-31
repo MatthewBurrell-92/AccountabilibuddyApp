4a38067acccfdcf01e65c91e8a92031271f89542
Matthew Burrell <bur17070@byui.edu>
2026-01-31 16:57:07 -0700

    Added createGoalView
    Played around with the background colors.

---
+++ Accountabilibuddy/Accountabilibuddy/createGoalView.swift
@@ -0,0 +1,38 @@
+//
+//  createGoalView.swift
+//  Accountabilibuddy
+//
+//  Created by Matthew Burrell on 1/31/26.
+//
+
+
+import SwiftUI
+
+struct createGoalView: View {
+    var body: some View {
+       ZStack {
+          Color.indigo
+              .ignoresSafeArea()
+          VStack {
+
+              Text("Create Goal!")
+                .background(Color.white)
+          }
+       }
+        
+        .padding()
+
+    }
+  
+               
+               // Your other content here
+               // Other layers will respect the safe area edges
+       
+   }
+
+
+   
+
+#Preview {
+   createGoalView()
+}
