782cc35924ba9f09f03c352837869b08a208da35
Matthew Burrell <bur17070@byui.edu>
2026-01-31 15:58:58 -0700

    Created XCode project

---
+++ Accountabilibuddy/Accountabilibuddy/AccountabilibuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccountabilibuddyApp.swift
+//  Accountabilibuddy
+//
+//  Created by Matthew Burrell on 1/31/26.
+//
+
+import SwiftUI
+
+@main
+struct AccountabilibuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Accountabilibuddy/AccountabilibuddyTests/AccountabilibuddyTests.swift
@@ -0,0 +1,17 @@
+//
+//  AccountabilibuddyTests.swift
+//  AccountabilibuddyTests
+//
+//  Created by Matthew Burrell on 1/31/26.
+//
+
+import Testing
+@testable import Accountabilibuddy
+
+struct AccountabilibuddyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
